d645bed856510d434af667788f5ebfe990e77436
skyhong2008 <skyhong2008@sina.com>
2012-12-21 10:20:19 +0800

    update cc mail

--- src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailService.java
+++ src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailService.java
@@ -1,8 +1,9 @@
 package com.juvenxu.mvnbook.account.email;
 
+public interface AccountEmailService {
+    void sendMail(String to, String subject, String htmlText)
+            throws AccountEmailException;
 
-public interface AccountEmailService
-{
-    void sendMail( String to, String subject, String htmlText )
-        throws AccountEmailException;
+    void sendMail(String to, String cc, String subject, String htmlText)
+            throws AccountEmailException;
 }
--- src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailServiceImpl.java
+++ src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailServiceImpl.java
@@ -1,57 +1,69 @@
 package com.juvenxu.mvnbook.account.email;
 
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
-public class AccountEmailServiceImpl
-    implements AccountEmailService
-{
+/**
+ * 
+ * @author huwg
+ * 
+ */
+public class AccountEmailServiceImpl implements AccountEmailService {
     private JavaMailSender javaMailSender;
 
     private String systemEmail;
 
-    public void sendMail( String to, String subject, String htmlText )
-        throws AccountEmailException
-    {
-        try
-        {
+    public void sendMail(String to, String subject, String htmlText)
+            throws AccountEmailException {
+        try {
             MimeMessage msg = javaMailSender.createMimeMessage();
-            MimeMessageHelper msgHelper = new MimeMessageHelper( msg );
+            MimeMessageHelper msgHelper = new MimeMessageHelper(msg);
 
-            msgHelper.setFrom( systemEmail );
-            msgHelper.setTo( to );
-            msgHelper.setSubject( subject );
-            msgHelper.setText( htmlText, true );
+            msgHelper.setFrom(systemEmail);
+            msgHelper.setTo(to);
+            msgHelper.setSubject(subject);
+            msgHelper.setText(htmlText, true);
 
-            javaMailSender.send( msg );
-        }
-        catch ( MessagingException e )
-        {
-            throw new AccountEmailException( "Faild to send mail.", e );
+            javaMailSender.send(msg);
+        } catch (MessagingException e) {
+            throw new AccountEmailException("Faild to send mail.", e);
         }
     }
 
-    public JavaMailSender getJavaMailSender()
-    {
+    public void sendMail(String to, String cc, String subject, String htmlText)
+            throws AccountEmailException {
+        try {
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            MimeMessageHelper msgHelper = new MimeMessageHelper(msg);
+
+            msgHelper.setFrom(systemEmail);
+            msgHelper.setTo(to);
+            msgHelper.setCc(cc);
+            msgHelper.setSubject(subject);
+            msgHelper.setText(htmlText, true);
+
+            javaMailSender.send(msg);
+        } catch (MessagingException e) {
+            throw new AccountEmailException("Faild to send mail.", e);
+        }
+    }
+
+    public JavaMailSender getJavaMailSender() {
         return javaMailSender;
     }
 
-    public void setJavaMailSender( JavaMailSender javaMailSender )
-    {
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
-    public String getSystemEmail()
-    {
+    public String getSystemEmail() {
         return systemEmail;
     }
 
-    public void setSystemEmail( String systemEmail )
-    {
+    public void setSystemEmail(String systemEmail) {
         this.systemEmail = systemEmail;
     }
 }
